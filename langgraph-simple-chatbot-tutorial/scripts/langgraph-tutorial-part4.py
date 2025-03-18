d63ea4c18d2990ba5b1c0e9342a75a2f906d9d69
Gilberto González Rodríguez <47121318+ginrod@users.noreply.github.com>
2025-03-18 08:15:46 -0700

    Human in the loop (#4)
    * add new code to request assistance from human
    
    * fix some details
    
    * send human expert feedback to the agent
    
    * add .py script

---
+++ langgraph-simple-chatbot-tutorial/scripts/langgraph-tutorial-part4.py
@@ -0,0 +1,61 @@
+from typing import Annotated
+
+from langchain_anthropic import ChatAnthropic
+from langchain_community.tools.tavily_search import TavilySearchResults
+from langchain_core.tools import tool
+from typing_extensions import TypedDict
+
+from langgraph.checkpoint.memory import MemorySaver
+from langgraph.graph import StateGraph, START, END
+from langgraph.graph.message import add_messages
+from langgraph.prebuilt import ToolNode, tools_condition
+from langgraph.types import Command, interrupt
+
+ANTHROPIC_API_KEY = "ANTHROPIC_API_KEY"
+
+os.environ["ANTHROPIC_API_KEY"] = ANTHROPIC_API_KEY
+
+TAVILY_API_KEY = "TAVILY_API_KEY"
+
+os.environ["TAVILY_API_KEY"] = TAVILY_API_KEY
+
+class State(TypedDict):
+    messages: Annotated[list, add_messages]
+
+
+graph_builder = StateGraph(State)
+
+
+@tool
+def human_assistance(query: str) -> str:
+    """Request assistance from a human."""
+    human_response = interrupt({"query": query})
+    return human_response["data"]
+
+
+tool = TavilySearchResults(max_results=2)
+tools = [tool, human_assistance]
+llm = ChatAnthropic(model="claude-3-5-sonnet-20240620")
+llm_with_tools = llm.bind_tools(tools)
+
+
+def chatbot(state: State):
+    message = llm_with_tools.invoke(state["messages"])
+    assert(len(message.tool_calls) <= 1)
+    return {"messages": [message]}
+
+
+graph_builder.add_node("chatbot", chatbot)
+
+tool_node = ToolNode(tools=tools)
+graph_builder.add_node("tools", tool_node)
+
+graph_builder.add_conditional_edges(
+    "chatbot",
+    tools_condition,
+)
+graph_builder.add_edge("tools", "chatbot")
+graph_builder.add_edge(START, "chatbot")
+
+memory = MemorySaver()
+graph = graph_builder.compile(checkpointer=memory)
